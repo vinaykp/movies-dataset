26454a3700b3f82eda570787331a9044cf318017
vinaykp <vinaykp@hotmail.com>
2024-09-21 04:12:12 +0000

    changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
+st.set_page_config(page_title="Movies dataset...", page_icon="🎬")
 st.title("🎬 Movies dataset")
 st.write(
     """
